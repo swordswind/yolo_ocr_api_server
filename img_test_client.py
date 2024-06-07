be1ca117f3445b2a8c1d8d4c4842d5049a516e10
枫影剑 <97500442+swordswind@users.noreply.github.com>
2024-06-07 22:36:06 +0800

    Update img_test_client.py

--- img_test_client.py
+++ img_test_client.py
@@ -3,27 +3,31 @@ import requests
 
 def get_result(choice):
     url = f"http://127.0.0.1:8089/{choice}"
-    # 发送请求
-    response = requests.get(url)
-    # 解析JSON响应
-    response_json = response.json()
-    # 返回图像识别模型的识别结果
-    return response_json["answer"]
+    response = requests.get(url)  # 发送请求
+    response_json = response.json()  # 解析JSON响应
+    return response_json["answer"]  # 返回图像识别模型的识别结果
 
 
 while True:
-    user_input = input("输入y按下回车进行YOLO摄像头画面识别测试，输入o后按下回车进行OCR屏幕识别测试:")
+    user_input = input("输入y按下回车进行YOLO摄像头物体识别测试，输入s按下回车进行YOLO屏幕物体识别测试，输入o后按下回车进行OCR屏幕文字识别测试:")
     if user_input == "y":
-        print("请求发送成功，等待YOLO摄像头画面识别...")
+        print("请求发送成功，等待YOLO摄像头物体识别...")
         try:
-            yolo_response = get_result("yolo")
-            print(f"YOLO摄像头画面识别结果: {yolo_response}")
+            yolocam_response = get_result("yolo")
+            print(f"YOLO摄像头物体识别结果: {yolocam_response}\n")
         except:
-            print("提示: 图像识别模型API服务器未开启或摄像头未连接。")
+            print("提示: 图像识别模型API服务器未开启或摄像头未连接。\n")
+    elif user_input == "s":
+        print("请求发送成功，等待YOLO屏幕物体识别...")
+        try:
+            yoloscreen_response = get_result("yoloscreen")
+            print(f"YOLO屏幕物体识别结果: {yoloscreen_response}\n")
+        except:
+            print("提示: 图像识别模型API服务器未开启。\n")
     elif user_input == "o":
-        print("请求发送成功，等待OCR屏幕识别...")
+        print("请求发送成功，等待OCR屏幕文字识别...")
         try:
             ocr_response = get_result("ocr")
-            print(f"OCR屏幕识别结果: {ocr_response}")
+            print(f"OCR屏幕文字识别结果: {ocr_response}\n")
         except:
-            print("提示: 图像识别模型API服务器未开启或摄像头未连接。")
+            print("提示: 图像识别模型API服务器未开启。\n")

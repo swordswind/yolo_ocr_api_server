b9b58a5091dc4967a67fd96dbe5c6cb72ea5acdc
枫影剑 <97500442+swordswind@users.noreply.github.com>
2024-06-07 22:35:34 +0800

    Update img_api_server.py

--- img_api_server.py
+++ img_api_server.py
@@ -5,45 +5,33 @@ import uvicorn
 from PIL import ImageGrab
 from easyocr import Reader
 from fastapi import FastAPI
-from ultralytics import YOLO
+from ultralytics import YOLOv10
 
 app = FastAPI()
-# 初始化摄像头
-cap = cv2.VideoCapture(0)
-# 加载YOLOv8模型
-model = YOLO("model/yolov8m.pt")  # 确保模型权重文件路径正确
-# 创建一个空字典用于统计物品个数
-object_count = {}
-name_map = {"person": "个人", "bicycle": "辆自行车", "car": "辆汽车", "motorcycle": "辆摩托车", "airplane": "架飞机",
-            "bus": "辆公共汽车", "train": "列火车", "truck": "辆卡车", "boat": "艘船", "traffic light": "个交通灯",
-            "fire hydrant": "个消防栓", "stop sign": "个停车标志", "parking meter": "个停车计时器", "bench": "张长椅",
-            "bird": "只鸟", "cat": "只猫", "dog": "只狗", "horse": "匹马", "sheep": "只绵羊", "cow": "头奶牛",
-            "bowl": "个碗", "elephant": "头大象", "bear": "只熊", "zebra": "只斑马", "giraffe": "只长颈鹿",
-            "backpack": "个背包", "umbrella": "把伞", "handbag": "个手提包", "tie": "条领带", "suitcase": "个行李箱",
-            "frisbee": "个飞盘", "skis": "副滑雪板", "snowboard": "块滑雪板", "sports ball": "个运动球",
-            "kite": "个风筝",
-            "fork": "把叉子", "baseball bat": "根棒球棒", "baseball glove": "个棒球手套", "skateboard": "块滑板",
-            "surfboard": "块冲浪板", "tennis racket": "个网球拍", "bottle": "个瓶子", "wine glass": "个酒杯",
-            "cup": "个杯子", "knife": "把刀子", "spoon": "把勺子", "banana": "根香蕉", "apple": "个苹果",
-            "sandwich": "个三明治", "orange": "个橙子", "broccoli": "棵西兰花", "carrot": "根胡萝卜",
-            "hot dog": "个热狗",
-            "pizza": "张比萨", "donut": "个甜甜圈", "cake": "块蛋糕", "chair": "把椅子", "couch": "张沙发",
-            "potted plant": "盆盆栽", "bed": "张床", "tv": "台显示器", "dining table": "张餐桌", "toilet": "个厕所",
-            "laptop": "台笔记本电脑", "mouse": "个鼠标", "remote": "个遥控器", "keyboard": "个键盘",
-            "cell phone": "部手机",
-            "microwave": "台微波炉", "oven": "台烤箱", "vase": "个花瓶", "sink": "个水槽", "refrigerator": "台冰箱",
-            "book": "本书", "clock": "个时钟", "toaster": "个烤面包机", "scissors": "把剪刀", "teddy bear": "个泰迪熊",
-            "hair drier": "个吹风机", "toothbrush": "把牙刷"}
+cap = cv2.VideoCapture(0)  # 初始化摄像头
+model = YOLOv10("model/yolov10m.pt")  # 加载YOLOv10模型,确保模型权重文件路径正确
+object_count = {}  # 创建一个空字典用于统计物品个数
+name_map = {"0": "个人", "1": "辆自行车", "2": "辆汽车", "3": "辆摩托车", "4": "架飞机", "5": "辆公共汽车",
+            "6": "列火车", "7": "辆卡车", "8": "艘船", "9": "个交通灯", "10": "个消防栓", "11": "个停车标志",
+            "12": "个停车计时器", "13": "张长椅", "14": "只鸟", "15": "只猫", "16": "只狗", "17": "匹马",
+            "18": "只绵羊", "19": "头奶牛", "20": "头大象", "21": "只熊", "22": "只斑马", "23": "只长颈鹿",
+            "24": "个背包", "25": "把伞", "26": "个手提包", "27": "条领带", "28": "个行李箱", "29": "个飞盘",
+            "30": "副滑雪板", "31": "块滑雪板", "32": "个运动球", "33": "个风筝", "34": "根棒球棒",
+            "35": "个棒球手套", "36": "块滑板", "37": "块冲浪板", "38": "个网球拍", "39": "个瓶子", "40": "个酒杯",
+            "41": "个杯子", "42": "把叉子", "43": "把刀子", "44": "把勺子", "45": "个碗", "46": "根香蕉",
+            "47": "个苹果", "48": "个三明治", "49": "个橙子", "50": "棵西兰花", "51": "根胡萝卜", "52": "个热狗",
+            "53": "张比萨", "54": "个甜甜圈", "55": "块蛋糕", "56": "把椅子", "57": "张沙发", "58": "盆盆栽",
+            "59": "张床", "60": "张餐桌", "61": "个厕所", "62": "台显示器", "63": "台笔记本电脑", "64": "个鼠标",
+            "65": "个遥控器", "66": "个键盘", "67": "部手机", "68": "台微波炉", "69": "台烤箱", "70": "个烤面包机",
+            "71": "个水槽", "72": "台冰箱", "73": "本书", "74": "个时钟", "75": "个花瓶", "76": "把剪刀",
+            "77": "个泰迪熊", "78": "个吹风机", "79": "把牙刷"}
 
 
-def recog_things():
+def yolo_camera():
     global object_count
-    # 读取一帧
-    ret, frame = cap.read()
-    # YOLOv8检测
-    results = model(frame)[0]
-    # 获取检测到的物体名称
-    names = results.names
+    ret, frame = cap.read()  # 读取一帧
+    results = model(frame)[0]  # YOLOv10检测
+    names = results.names  # 获取检测到的物体名称
     detected_objects = []  # 创建一个空列表来存储检测到的物体名称
     for result in results:
         boxes = result.boxes
@@ -53,48 +41,68 @@ def recog_things():
             if name in name_map:
                 name = name_map[name]
                 detected_objects.append(name)
-                # 统计物品个数
-                object_count[name] = object_count.get(name, 0) + 1
-    # 输出规范化统计结果
-    count_output = '、'.join([f'{count}{name}' for name, count in object_count.items()])
+                object_count[name] = object_count.get(name, 0) + 1  # 统计物品个数
+    count_output = '、'.join([f'{count}{name}' for name, count in object_count.items()])  # 输出规范化统计结果
     object_count = {}  # 清零计数
     return count_output
 
 
-def recog_screen():
+def yolo_screen():
+    global object_count
+    width, height = pyautogui.size()  # 使用pyautogui来获取屏幕大小
+    screenshot = ImageGrab.grab(bbox=(0, 0, width, height))  # 截图整个屏幕
+    screenshot.save("cache.jpg", "JPEG")  # 保存截图到文件
+    results = model("cache.jpg")[0]  # YOLOv10检测
+    names = results.names  # 获取检测到的物体名称
+    detected_objects = []  # 创建一个空列表来存储检测到的物体名称
+    for result in results:
+        boxes = result.boxes
+        for box in boxes:
+            cls = box.cls.item()
+            name = names[int(cls)]
+            if name in name_map:
+                name = name_map[name]
+                detected_objects.append(name)
+                object_count[name] = object_count.get(name, 0) + 1  # 统计物品个数
+    count_output = '、'.join([f'{count}{name}' for name, count in object_count.items()])  # 输出规范化统计结果
+    object_count = {}  # 清零计数
+    os.remove("cache.jpg")
+    return count_output
+
+
+def ocr_screen():
     text = ""
-    # 使用pyautogui来获取屏幕大小
-    width, height = pyautogui.size()
-    # 截图整个屏幕
-    screenshot = ImageGrab.grab(bbox=(0, 0, width, height))
-    # 保存截图到文件
-    screenshot.save("cache.jpg", "JPEG")
-    # 使用EasyOCR进行OCR识别
-    reader = Reader(['ch_sim', 'en'], model_storage_directory="model")
+    width, height = pyautogui.size()  # 使用pyautogui来获取屏幕大小
+    screenshot = ImageGrab.grab(bbox=(0, 0, width, height))  # 截图整个屏幕
+    screenshot.save("cache.jpg", "JPEG")  # 保存截图到文件
+    reader = Reader(['ch_sim', 'en'], model_storage_directory="model")  # 使用EasyOCR进行OCR识别
     result = reader.readtext("cache.jpg")
-    # 初始化一个临时字符串用于存储本次识别的文本
-    temp_text = ""
-    # 遍历识别结果
-    for bbox, ocr_text, prob in result:
-        if prob >= 0.75:  # 只考虑概率大于等于75%的结果
+    temp_text = ""  # 初始化一个临时字符串用于存储本次识别的文本
+    for bbox, ocr_text, prob in result:  # 遍历识别结果
+        if prob >= 0.50:  # 只考虑概率大于等于75%的结果
             temp_text += ocr_text + " "  # 将识别到的文本追加到临时字符串，并加上空格分隔
-    # 将本次识别的文本追加到全局 text 变量中，并加上换行符分隔
-    text += temp_text + " "
+    text += temp_text + " "  # 将本次识别的文本追加到全局 text 变量中，并加上换行符分隔
     os.remove("cache.jpg")
     return text
 
 
 @app.get("/yolo")  # 使用举例：http://127.0.0.1:8089/yolo
-async def get_yolo_result():
-    answer = recog_things()
+async def get_yolocam_result():
+    answer = yolo_camera()
+    return {"answer": answer}
+
+
+@app.get("/yoloscreen")  # 使用举例：http://127.0.0.1:8089/yoloscreen
+async def get_yoloscreen_result():
+    answer = yolo_screen()
     return {"answer": answer}
 
 
 @app.get("/ocr")  # 使用举例：http://127.0.0.1:8089/ocr
 async def get_ocr_result():
-    answer = recog_screen()
+    answer = ocr_screen()
     return {"answer": answer}
 
 
-print("本地图像识别模型api服务器启动成功!")
+print("本地图像识别模型API服务器启动成功!")
 uvicorn.run(app, host="0.0.0.0", port=8089)
